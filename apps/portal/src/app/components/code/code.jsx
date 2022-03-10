9095195eeb276daaf85992a9501442cbb65676e5
Luke Rawlings <luke.rawlings@cityfibre.com>
2022-03-10 21:39:24 +0000

    change highlight.js to prism to reduce bundle size

--- apps/portal/src/app/components/code/code.jsx
+++ apps/portal/src/app/components/code/code.jsx
@@ -1,26 +1,23 @@
 import { chakra } from "@chakra-ui/react";
-import { Prism as Snippet } from "react-syntax-highlighter"
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import theme from 'prism-react-renderer/themes/nightOwl'
+import Highlight, { defaultProps } from "prism-react-renderer"
 
-const Code = ({ children, ...props }) => {
-  return (
-    <chakra.div position="relative">
-      <Snippet {...props}>
-        {children}
-      </Snippet>
-    </chakra.div>
-  )
-}
-
-Code.defaultProps = {
-  style: {
-    ...a11yDark,
-    "pre[class*=\"language-\"]": {
-      ...a11yDark["pre[class*=\"language-\"]"],
-      borderRadius: "8px",
-      padding: "1.5rem 2rem .5rem",
-    }
-  }
-}
+const Code = ({ children, ...props }) => (
+  <chakra.div position="relative">
+    <Highlight {...defaultProps} code={children} theme={theme} {...props}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <chakra.pre className={className} style={style} p={8} rounded={5}>
+          {tokens.map((line, i) => (
+            <chakra.div {...getLineProps({ line, key: i })} lineHeight={1.5}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </chakra.div>
+          ))}
+        </chakra.pre>
+      )}
+    </Highlight>
+  </chakra.div>
+)
 
 export default Code
