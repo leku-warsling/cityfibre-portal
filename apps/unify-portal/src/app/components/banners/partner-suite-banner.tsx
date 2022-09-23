0fec9ab0e55789e9d3dfa7d220ebcaccda487261
Luke Rawlings <luke.rawlings@cityfibre.com>
2022-09-23 16:19:54 +0100

    chore: change logo color login auth screens

--- apps/unify-portal/src/app/components/banners/partner-suite-banner.tsx
+++ apps/unify-portal/src/app/components/banners/partner-suite-banner.tsx
@@ -1,31 +1,33 @@
 import { Figure, Logo, CirclesBackdrop } from "@ui/lib"
 import { Heading, Text } from "@chakra-ui/layout"
-import { useToken } from "@chakra-ui/system"
 
-export const PartnerSuiteBanner = () => {
-  const primaryColor = useToken("colors", "primary.500")
-  return (
-    <Figure
-      bgColor="secondary.500"
-      bgImage={CirclesBackdrop}
-      bgPos="center"
-      bgSize="cover"
-      h="100vh"
-      w="50%"
-    >
-      <Figure.Header pt={{ lg: 10, md: 8 }} pl={{ lg: 10, md: 8 }}>
-        <Logo height="48" fill={primaryColor} />
-      </Figure.Header>
-      <Figure.Caption pl={{ lg: 10, md: 8 }} pb={12} gap={4} pr={{ md: 8 }}>
-        <Heading fontSize={{ lg: "3xl", md: "xl" }}>
-          CityFibre Partner Suite
-        </Heading>
-        <Text maxW="500px">
-          Serve your customers quickly and effectively from everything to
-          ordering products and services for businesses to managing and
-          resolving service issues
-        </Text>
-      </Figure.Caption>
-    </Figure>
-  )
-}
+export const PartnerSuiteBanner = () => (
+  <Figure
+    bgColor="secondary.500"
+    bgImage={CirclesBackdrop}
+    bgPos="center"
+    bgSize="cover"
+    h="100vh"
+    w="50%"
+  >
+    <Figure.Header pt={{ lg: 10, md: 8 }} pl={{ lg: 10, md: 8 }}>
+      <Logo
+        height="48"
+        fill="#fff"
+        style={{
+          filter: "drop-shadow(0px 2px 2px rgb(0 0 0 / 0.15)",
+        }}
+      />
+    </Figure.Header>
+    <Figure.Caption pl={{ lg: 10, md: 8 }} pb={12} gap={4} pr={{ md: 8 }}>
+      <Heading fontSize={{ lg: "3xl", md: "xl" }}>
+        CityFibre Partner Suite
+      </Heading>
+      <Text maxW="500px">
+        Serve your customers quickly and effectively from everything to ordering
+        products and services for businesses to managing and resolving service
+        issues
+      </Text>
+    </Figure.Caption>
+  </Figure>
+)
