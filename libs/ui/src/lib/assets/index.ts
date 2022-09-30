0435393394810737ade6b3ed0bda6137819a8e7e
Luke Rawlings <luke.rawlings@cityfibre.com>
2022-09-30 13:37:33 +0100

    fix logo and background image on auth banner

--- libs/ui/src/lib/assets/index.ts
+++ libs/ui/src/lib/assets/index.ts
@@ -1,6 +1,6 @@
 // export { ReactComponent as PartnersIcon } from "./svg/partners.svg"
 export { default as loginBackdrop } from "./img/login-bg.jpg"
-// export { default as CirclesBackdrop } from "./svg/circles.svg"
+export { default as circlesBackdrop } from "./svg/circles.svg"
 // export { ReactComponent as Logo } from "./svg/logo.svg"
 
 export { default as PartnersIcon } from "./icons/partners.icon"
